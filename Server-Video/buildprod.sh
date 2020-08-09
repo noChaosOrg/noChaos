463a79f735eb08f8a0d99d944d6f1b75b1f4e2fa
noChaos1012 <waschild@163.com>
2020-08-10 01:13:54 +0800

    video: 修改buildprod脚本

--- Server-Video/buildprod.sh
+++ Server-Video/buildprod.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
 cd ~/go/src/github.com/noChaos1012/noChaos/Server-Video/api
-env GOOS=linux GOARCH=amd64 go build -o ../bin/api
+env GOOS=linux GOARCH=amd64 go build -o ../bin/publish/api
 
 cd ~/go/src/github.com/noChaos1012/noChaos/Server-Video/scheduler
-env GOOS=linux GOARCH=amd64 go build -o ../bin/scheduler
+env GOOS=linux GOARCH=amd64 go build -o ../bin/publish/scheduler
 
 cd ~/go/src/github.com/noChaos1012/noChaos/Server-Video/streamserver
-env GOOS=linux GOARCH=amd64 go build -o ../bin/streamserver
+env GOOS=linux GOARCH=amd64 go build -o ../bin/publish/streamserver
 
 cd ~/go/src/github.com/noChaos1012/noChaos/Server-Video/web
-env GOOS=linux GOARCH=amd64 go build -o ../bin/web
+env GOOS=linux GOARCH=amd64 go build -o ../bin/publish/web
 
+echo "buildprod finished"
